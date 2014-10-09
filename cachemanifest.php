16093229ae5f48638531e2c5707988cc91b4d3ea
Jannis Grimm <jannis@gje.ch>
2014-10-09 19:58:02 +0200

    Now with 100 % more Open Source!

---
+++ cachemanifest.php
@@ -0,0 +1,12 @@
+<?php
+	header('Content-Type: text/cache-manifest; charset=utf-8');
+?>
+CACHE MANIFEST
+
+# Generator v5
+# Headlines v14_1
+
+index.php
+scripts.js
+followers.json
+headlines.json
